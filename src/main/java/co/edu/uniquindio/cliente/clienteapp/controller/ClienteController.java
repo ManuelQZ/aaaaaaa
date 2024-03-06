57db36cfb9aea30e5c2d00718e2f15607b625f0d
Spalax-dev <jorgity345@gmail.com>
2024-03-06 00:40:20 -0500

    Se elimino cosas innecesarias

--- src/main/java/co/edu/uniquindio/cliente/clienteapp/controller/ClienteController.java
+++ src/main/java/co/edu/uniquindio/cliente/clienteapp/controller/ClienteController.java
@@ -1,82 +1,96 @@
-//package co.edu.uniquindio.cliente.clienteapp.controller;
-//
-//import java.net.URL;
-//import java.util.ResourceBundle;
-//
-//import co.edu.uniquindio.cliente.clienteapp.model.Cliente;
-//import javafx.event.ActionEvent;
-//import javafx.fxml.FXML;
-//import javafx.scene.control.*;
-//
-//public class ClienteController {
-//
-//    @FXML
-//    private ResourceBundle resources;
-//
-//    @FXML
-//    private URL location;
-//
-//    @FXML
-//    private TextField txtNombre;
-//
-//    @FXML
-//    private TextField txtCelular;
-//
-//    @FXML
-//    private TextField txtEdad;
-//
-//    @FXML
-//    private TextField txtApellido;
-//
-//    @FXML
-//    private TextField txtTelefonoFijo;
-//
-//    @FXML
-//    private TextField txtCedula;
-//
-//    @FXML
-//    private TextArea txtResultado;
-//
-//    @FXML
-//    private ToggleGroup grupo1;
-//
-//    @FXML
-//    private RadioButton rButtonClienteBasico;
-//
-//    @FXML
-//    private RadioButton rButtonClientePremium;
-//
-//    @FXML
-//    private RadioButton rButtonClienteVip;
-//
-//    @FXML
-//    private Button btnAgregarCliente;
-//
-//    @FXML
-//    void onAgregarCliente(ActionEvent event) {
-//        agregarCliente();
-//    }
-//
-//    @FXML
-//    void initialize() {
-//    }
-//
-//    private void agregarCliente() {
-//        Cliente cliente = new Cliente();
-//        cliente.setNombre(txtNombre.getText());
-//        cliente.setApellido(txtApellido.getText());
-//        cliente.setCedula(txtCedula.getText());
-//        cliente.setEdad(Integer.parseInt(txtEdad.getText()));
-//        cliente.setTelefonoFijo(txtTelefonoFijo.getText());
-//        cliente.setTelefonoCelular(txtCelular.getText());
-//        txtResultado.setText(cliente.toString());
-//
-//        ToggleGroup grupoTipoCliente = null;
-//        RadioButton selectedRadioButton = (RadioButton) grupoTipoCliente.getSelectedToggle();
-//        String tipoCliente = selectedRadioButton.getText();
-//        cliente.setTipoCliente(tipoCliente);
-//
-//        txtResultado.setText(cliente.toString());
-//
-//    }
-//}
+package co.edu.uniquindio.cliente.clienteapp.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import co.edu.uniquindio.cliente.clienteapp.model.Cliente;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class ClienteController {
+
+  private Cliente resultado;
+
+   @FXML
+   private ResourceBundle resources;
+
+   @FXML
+   private URL location;
+
+   @FXML
+   private TextField txtNombre;
+
+   @FXML
+   private TextField txtCelular;
+
+   @FXML
+   private TextField txtEdad;
+
+   @FXML
+   private TextField txtApellido;
+
+   @FXML
+   private TextField txtTelefonoFijo;
+
+   @FXML
+   private TextField txtCedula;
+
+   @FXML
+   private TextArea txtResultado;
+
+   @FXML
+   private ToggleGroup grupo1;
+
+   @FXML
+   private RadioButton rButtonClienteBasico;
+
+   @FXML
+   private RadioButton rButtonClientePremium;
+
+   @FXML
+   private RadioButton rButtonClienteVip;
+
+   @FXML
+   private Button btnAgregarCliente;
+
+   @FXML
+   void onAgregarCliente(ActionEvent event) {
+       agregarCliente();
+   }
+
+   @FXML
+   void initialize() {
+   }
+
+   private void agregarCliente() {
+      
+      ClaseCliente tiposCliente = new ClienteCliente();
+
+      String nombre = txtNombre.getText();
+      int edad = Integer.parseInt(txtEdad.getText()
+      String apellido = txtApellido.getText();
+      String numeroCelular = txtCelular.getText();
+      String cedula = txtCedula.getText();
+      String numeroTelefono = txtTelefonoFijo.getText();
+
+        
+      if (rButtonClienteBasico.isSelected() == true) {
+          if (nombre != null && edad != 0 && apellido != null && cedula != null) {
+            Cliente resultado = tiposCliente.clienteBasic(nombre, edad, apellido, cedula);
+          }
+          
+      } else if(rButtonClientePremium.isSelected() == true) {
+          if (nombre != null && edad != 0 && apellido != null && cedula != null) {
+            Cliente resultado = tiposCliente.clientePremium(nombre, edad, apellido, cedula);
+          }
+
+      } else if(rButtonClienteVip.isSelected() == true) {
+          if (nombre != null && edad != 0 && apellido != null && cedula != null) {
+            Cliente resultado = tiposCliente.clienteVip(nombre, edad, apellido, cedula);
+          }
+      } else {
+      }
+      txtResultado.setText(resultado.toString());
+   }
+}
